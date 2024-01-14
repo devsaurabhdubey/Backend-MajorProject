b054c1e61ebe22512b802a61498d54f05ae032bf
devsaurabhdubey <37721387+devsaurabhdubey@users.noreply.github.com>
2024-01-14 12:35:43 +0530

    routers updated

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req,res){
+    return res.end("<h1>Module is up for Codial !!</h1>")
+}
+
+module.exports.information = function(req,res){
+   return res.end("<h1>tHE INFORMATION iS WORKING FIKNE !!</h1>");
+}
