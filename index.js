fe5ea3fdbcb9828d361a48171cdd2dca73649c3a
devsaurabhdubey <37721387+devsaurabhdubey@users.noreply.github.com>
2024-01-26 10:41:00 +0530

    Updating the mongoose configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const expressLayouts = require("express-ejs-layouts");
+const db = require('./config/mongoose');
 
 const port = 8000;
 
